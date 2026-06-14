af5c869ae7095109d5a527b61f84d95b893d4116
McZyWu <zhuoyun.wu.23@ucl.ac.uk>
2026-06-14 22:29:38 +0800

    lint checks

--- test/registered/ascend/performance/qwen3_6_35b_a3b/test_npu_qwen3_6_35b_a3b_1p_in64k_out1k_prefix90_50ms_aime26.py
+++ test/registered/ascend/performance/qwen3_6_35b_a3b/test_npu_qwen3_6_35b_a3b_1p_in64k_out1k_prefix90_50ms_aime26.py
@@ -50,11 +50,11 @@ QWEN3_6_35B_A3B_64K_PREFIX_OTHER_ARGS = [
     "--mamba-scheduler-strategy",
     "extra_buffer",
     "--max-running-requests",
-    48,
+    44,
     "--max-mamba-cache-size",
-    240,
+    220,
     "--mem-fraction-static",
-    0.71,
+    0.8,
     "--cuda-graph-bs",
     2,
     8,
@@ -64,7 +64,6 @@ QWEN3_6_35B_A3B_64K_PREFIX_OTHER_ARGS = [
     36,
     40,
     44,
-    48,
     "--enable-multimodal",
     "--mm-attention-backend",
     "ascend_attn",
@@ -93,8 +92,8 @@ class TestNPUQwen3_6_35BA3B_1P_In64k_Out1k_Prefix90_50ms(
     other_args = QWEN3_6_35B_A3B_64K_PREFIX_OTHER_ARGS
     envs = QWEN3_6_35B_A3B_64K_PREFIX_ENVS
     dataset_name = "generated-shared-prefix"
-    max_concurrency = 48
-    num_prompts = 48
+    max_concurrency = 44
+    num_prompts = 44
     input_len = 65536
     output_len = 1024
     random_range_ratio = 1
