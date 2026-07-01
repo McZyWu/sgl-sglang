334750f1289dd23da476a86b9827c16512dc96f8
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-07-01 18:01:14 +0800

    Update test_npu_deepep.py

--- test/registered/ascend/basic_function/parallel_strategy/expert_parallelism/test_npu_deepep.py
+++ test/registered/ascend/basic_function/parallel_strategy/expert_parallelism/test_npu_deepep.py
@@ -4,6 +4,7 @@ from types import SimpleNamespace
 from urllib.parse import urlparse
 
 from sglang.srt.utils import kill_process_tree
+from sglang.test.ascend.test_ascend_utils import DEEPSEEK_R1_0528_W8A8_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.few_shot_gsm8k import run_eval as run_eval_few_shot_gsm8k
 from sglang.test.test_utils import (
