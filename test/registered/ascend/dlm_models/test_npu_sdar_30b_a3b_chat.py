3173314f14bc91cf32afdf6d3460d3e140d82299
cy <chenyang08056032@163.com>
2026-07-07 20:05:21 +0800

    Add new model testcase

--- test/registered/ascend/dlm_models/test_npu_sdar_30b_a3b_chat.py
+++ test/registered/ascend/dlm_models/test_npu_sdar_30b_a3b_chat.py
@@ -5,7 +5,7 @@ from sglang.test.ascend.test_ascend_utils import SDAR_30B_A3B_CHAT_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import CustomTestCase
 
-register_npu_ci(est_time=400, suite="full-1-npu-a3", nightly=True)
+register_npu_ci(est_time=400, suite="full-2-npu-a3", nightly=True)
 
 
 class TestAFM(GSM8KAscendMixin, CustomTestCase):
@@ -17,6 +17,16 @@ class TestAFM(GSM8KAscendMixin, CustomTestCase):
 
     model = SDAR_30B_A3B_CHAT_WEIGHTS_PATH
     accuracy = 0
+    other_args = [
+        "--trust-remote-code",
+        "--mem-fraction-static",
+        "0.8",
+        "--attention-backend",
+        "ascend",
+        "--disable-cuda-graph",
+        "--tp-size",
+        2,
+    ]
 
 
 if __name__ == "__main__":
